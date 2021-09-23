f25d0a807e75bbc4ba2f30b0cda3cb16c744e4f8
tymruk_yurii <tymruk38@gmail.com>
2021-09-23 09:25:50 +0300

    main.js-const

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-let applePrice = 15.678;
-let lemonPrice = 123.965;
-let orangePrice = 90.2345;
+const applePrice = 15.678;
+const lemonPrice = 123.965;
+const orangePrice = 90.2345;
 
 // Завдання 1
 console.log(Math.max(applePrice, lemonPrice, orangePrice));
@@ -9,7 +9,7 @@ console.log(Math.max(applePrice, lemonPrice, orangePrice));
 console.log(Math.min(applePrice, lemonPrice, orangePrice));
 
 // Завдання 3
-let allSumGoods = applePrice + lemonPrice + orangePrice;
+const allSumGoods = applePrice + lemonPrice + orangePrice;
 console.log(allSumGoods);
 
 // Завдання 4
@@ -21,7 +21,7 @@ console.log(Math.round(allSumGoods / 100) * 100);
 console.log(Math.round(Math.floor(allSumGoods) % 2 === 0));
 
 // Завдання 7
-let sum = 500;
+const sum = 500;
 console.log(sum - allSumGoods);
 
 // Завдання 8
@@ -29,6 +29,6 @@ const averagePrice = allSumGoods / 3;
 console.log(Math.round(averagePrice * 100) / 100);
 
 // Завдання 9
-let randomSale = Math.floor(Math.random() * 100);
+const randomSale = Math.floor(Math.random() * 100);
 console.log(randomSale);
 console.log(Math.round((allSumGoods / 2 - (allSumGoods * randomSale) / 100) * 100) / 100);
