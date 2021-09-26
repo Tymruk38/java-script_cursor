76909e0b48879ce387b200b56d0bfd3756603c1e
tymruk_yurii <tymruk38@gmail.com>
2021-09-26 19:15:24 +0300

    new js

--- js2/main.js
+++ js2/main.js
@@ -16,21 +16,3 @@ for (let i = N; i <= M; i++){
     sum += i;
 }
 alert(`Result: ${sum}`);
-
-  
-// let N = parseInt(prompt('Enter the number N:'));
-
-// let M = parseInt(prompt('Enter the number M:'));
-
-// let skipEvenNumber = confirm('Should I skip even numbers?');
-
-// let result = 0;
-
-// for (let i = N; i <= M; i++) {
-//     if (skipEvenNumber && !(i % 2)) {
-//         continue;
-//     }
-//     result += i;
-// }
-
-// alert(`Result: ${result}`);
