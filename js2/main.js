dfaa26d163a182f888551c5f6df6781a72d836ab
tymruk_yurii <tymruk38@gmail.com>
2021-09-27 20:03:13 +0300

    mod file js

--- js2/main.js
+++ js2/main.js
@@ -1,15 +1,42 @@
-let N = parseInt(prompt("Hi, enter a number N"));
-console.log(N);
+// let N = parseInt(prompt("Hi, enter a number N"));
+// console.log(N);
 
-let M = parseInt(prompt("Hi, enter a number M"));
-console.log(M);
+// let M = parseInt(prompt("Hi, enter a number M"));
+// console.log(M);
+
+// let evenNumbers = confirm("Skip even numbers? ");
+// console.log(evenNumbers);
+
+// let sum = 0;
+
+// for (let i = N; i <= M; i++){
+//     if (evenNumbers && !(i % 2)) {
+//         continue;
+//     }
+//     sum += i;
+// }
+// alert(`Result: ${sum}`);
+let n;
+
+do {
+    n = parseInt(prompt("Hi, enter a number N"));
+} while (Number.isNaN(n));
+
+console.log(n);
+
+let m;
+do {
+    m = parseInt(prompt("Hi, enter a number M"));
+} while (Number.isNaN(m) && m > n);
+
+console.log(m);
 
 let evenNumbers = confirm("Skip even numbers? ");
 console.log(evenNumbers);
 
 let sum = 0;
 
-for (let i = N; i <= M; i++){
+for (let i = n; i <= m; i++) {
     if (evenNumbers && !(i % 2)) {
         continue;
     }
