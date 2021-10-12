449f41e77fb58f83216e4e1f210600fc0c165c22
tymruk_yurii <tymruk38@gmail.com>
2021-10-12 19:44:12 +0300

    add js6

--- js6/main6.js
+++ js6/main6.js
@@ -32,7 +32,9 @@ const students = [
 const getSubjects = (subject) => {
   let subjects = [];
   for (let key in subject) {
-    const subjectn = (key.substring(0, 1).toUpperCase() + key.substring(1).toLowerCase()).replace("_", " ");
+    const subjectn = (
+      key.substring(0, 1).toUpperCase() + key.substring(1).toLowerCase()
+    ).replace("_", " ");
     subjects.push(subjectn);
   }
   return subjects;
@@ -41,42 +43,64 @@ const getSubjects = (subject) => {
 console.log(getSubjects(students[0].subjects));
 
 // Функція 2
-const getAverageMark = (students) =>{
-   const markSubject = Object.values(students.subjects);
-   const allMarkSubject = markSubject[0].concat(markSubject[1], markSubject[2]);
-   const averageMark = (allMarkSubject.reduce((n1, n2)=> n1 + n2));
-   return +(averageMark /  allMarkSubject.length).toFixed(2)
-}
-console.log(getAverageMark(students[1]))
-
+const getAverageMark = (students) => {
+  const markSubject = Object.values(students.subjects);
+  const allMarkSubject = markSubject[0].concat(markSubject[1], markSubject[2]);
+  const averageMark = allMarkSubject.reduce((n1, n2) => n1 + n2);
+  return +(averageMark / allMarkSubject.length).toFixed(2);
+};
+console.log(getAverageMark(students[1]));
 
 // Функція 3
 
-const getStudentInfo = (students) =>  {
+const getStudentInfo = (students) => {
   const studentInfo = {
-    course : students.course,
-    name : students.name,
-    averageMark : getAverageMark(students),
+    course: students.course,
+    name: students.name,
+    averageMark: getAverageMark(students),
   };
-  return studentInfo
-}
-console.log( getStudentInfo(students[1]))
+  return studentInfo;
+};
+console.log(getStudentInfo(students[1]));
 
 // Функція 4
-const getStudentsNames = (studentsNames) =>{
-  const name = students.map(item => item.name).sort((a, b) => {
-    if (a > b) {
-          return 1;
-        }
-        if (a < b) {
-          return -1;
-        }
-        return 0;
-  })
-  return name
-}
-console.log(getStudentsNames(students))
+const getStudentsNames = (studentsNames) => {
+  const name = students
+    .map((item) => item.name)
+    .sort((a, b) => {
+      if (a > b) {
+        return 1;
+      }
+      if (a < b) {
+        return -1;
+      }
+      return 0;
+    });
+  return name;
+};
+console.log(getStudentsNames(students));
 
 // Функція 5
 
+const getBestStudent = (students) => {
+  let bestStudent = students.reduce((a, b) =>
+    getAverageMark(a) > getAverageMark(b) ? a : b
+  ).name;
+  return bestStudent;
+};
+console.log(getBestStudent(students));
 
+// Функція 6
+
+const calculateWordLetters = (word) => {
+  const lowerCaseLetters = word.toLowerCase().split("");
+  const objWord = {};
+
+  lowerCaseLetters.map((letter) =>
+    !objWord[letter] ? (objWord[letter] = 1) : objWord[letter]++
+  );
+
+  return objWord;
+};
+
+console.log(calculateWordLetters("курсор"));
