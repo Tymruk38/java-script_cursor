4279e43d74c130ffdb7817baa1479ae58b6842f7
tymruk_yurii <tymruk38@gmail.com>
2021-10-27 21:23:32 +0300

    add feal

--- js 9/main9.js
+++ js 9/main9.js
@@ -1,19 +1,31 @@
-const container = document.querySelector(".container_flex");
+const body = document.body;
+const container = document.createElement("div");
+container.className = "container";
+container.getAttribute = "style";
+document.body.append(container);
 container.style.width = "250px";
 container.style.height = "250px";
 container.style.display = "flex";
 container.style.flexWrap = "wrap";
 container.style.margin = "50px auto";
 const generateBlocks = () => {
-  for (const square of container.children) {
-    const r = Math.round(Math.random() * 255);
-    const g = Math.round(Math.random() * 255);
-    const b = Math.round(Math.random() * 255);
-    square.style.backgroundColor = `rgb(${r}, ${b}, ${g})`;
+  const squareSize = 50;
+  for (let i = 0; i < 25; i++) {
+    const square = document.createElement("div");
+    square.className = "square";
+    const left = i % 5;
+    const top = (i - left) / 5;
+    square.style.left = `${left * squareSize}px`;
+    square.style.top = `${top * squareSize}px`;
+    square.style.background =
+      "#" + (0x1000000 + Math.random() * 0xffffff).toString(16).substr(1, 6);
+    container.append(square);
   }
 };
 generateBlocks();
+
 const generateBlocksInterval = () => {
-  setInterval(() => generateBlocks(), 1000);
+  setInterval(generateBlocks, 1000);
 };
+
 generateBlocksInterval();
