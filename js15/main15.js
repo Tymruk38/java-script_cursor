9b62a5feba8fe61c0775d151b49dee283280eabd
tymruk_yurii <tymruk38@gmail.com>
2021-11-07 19:49:11 +0200

    add js15

---
+++ js15/main15.js
@@ -0,0 +1,12 @@
+function* idGenerator() {
+    let i = 1;
+    while (true) {
+        yield i++;
+    }
+}
+const id = idGenerator();
+console.log(id.next().value);
+console.log(id.next().value);
+console.log(id.next().value);
+console.log(id.next().value);
+console.log(id.next().value);
