db854ae77276a7b49b9ddf264f59287749ed776c
tymruk_yurii <tymruk38@gmail.com>
2021-11-08 14:41:38 +0200

    add new style15.css

--- js15/main15.js
+++ js15/main15.js
@@ -1,8 +1,8 @@
 function* idGenerator() {
-    let i = 1;
-    while (true) {
-        yield i++;
-    }
+  let i = 1;
+  while (true) {
+    yield i++;
+  }
 }
 const id = idGenerator();
 console.log(id.next().value);
